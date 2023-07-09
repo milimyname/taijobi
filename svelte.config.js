4b178c37dd29d492b4811c322fdb4169fc0fe5d1
mili <kamil.maksudov2019@gmail.com>
2023-07-09 05:09:03 +0200

    feat: add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
