e309b9daceee1eac4728c8d739c4605e97384e96
mili <kamil.maksudov2019@gmail.com>
2023-07-09 05:15:35 +0200

    feat: toggle adapters

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
-import adapter from '@sveltejs/adapter-vercel';
-// import adapter from '@sveltejs/adapter-node';
+import vercel from '@sveltejs/adapter-vercel';
+import node from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +12,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: process.env.VERCEL ? vercel() : node()
 	}
 };
 
