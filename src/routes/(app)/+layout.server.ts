ede0b4a332c0cf763f5cce3ccb42bdfe00dce261
mili <kamil.maksudov2019@gmail.com>
2023-08-05 16:30:41 +0200

    bug: too many redirects

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -3,7 +3,6 @@ import { redirect } from '@sveltejs/kit';
 export const load = async ({ locals }) => {
 	// Redirect if already logged in
 	if (!locals.pb.authStore.isValid) throw redirect(303, '/login');
-
 	return {
 		user: structuredClone(locals.pb.authStore.model)
 	};
---
+++ src/routes/drawing/+layout.server.ts
@@ -0,0 +1,9 @@
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ locals }) => {
+	// Redirect if already logged in
+	if (!locals.pb.authStore.isValid) throw redirect(303, '/login');
+	return {
+		user: structuredClone(locals.pb.authStore.model)
+	};
+};
