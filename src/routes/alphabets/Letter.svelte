c4c58daaa7e44e8a896f20c9d63e0e9a9e4c4dd5
mili <kamil.maksudov2019@gmail.com>
2023-08-18 19:58:57 +0200

    feat: added grade 2 kanji

--- src/routes/alphabets/Letter.svelte
+++ src/routes/alphabets/Letter.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { kanjiStore } from './../../lib/utils/stores.ts';
+	import { kanjiStore } from '$lib/utils/stores';
 	import {
 		animateSVG,
 		currentLetter,
@@ -21,29 +21,22 @@
 
 	// Determine which object (hiragana, katakana, kanji) to use based on the URL path
 	let currentObject: any;
-	switch ($currentAlphabet) {
-		case 'katakana':
-			currentObject = katakana;
-			break;
-		case 'kanji':
-			currentObject = kanji;
-			break;
-		default:
-			currentObject = hiragana;
-	}
 
 	$: switch ($currentAlphabet) {
 		case 'katakana':
 			$currentLetter = $katakanaStore[$progressSlider - 1];
 			viewBox = '0 0 1024 1024';
+			currentObject = katakana;
 			break;
 		case 'kanji':
 			$currentLetter = $kanjiStore[$progressSlider - 1];
 			viewBox = '0 0 109 109';
+			currentObject = kanji;
 			break;
 		default:
 			$currentLetter = $hiraganaStore[$progressSlider - 1];
 			viewBox = '0 0 80 87';
+			currentObject = hiragana;
 	}
 </script>
 
@@ -51,7 +44,7 @@
 	xmlns="http://www.w3.org/2000/svg"
 	{viewBox}
 	fill="none"
-	class="absolute left-1/2 top-[45%] sm:left-[55%] sm:top-1/2 {rotationY > 30
+	class="absolute left-1/2 top-[45%] sm:left-[55%] sm:top-1/2 {rotationY > 5
 		? 'hidden'
 		: 'block'}  max-w-80 max-h-80 -translate-x-1/2 -translate-y-1/2 opacity-20 sm:-translate-y-1/2"
 >
@@ -60,7 +53,7 @@
 			<path
 				d={path}
 				stroke="black"
-				stroke-width={$currentAlphabet === 'katakana' ? '60' : '3'}
+				stroke-width={$currentAlphabet === 'katakana' ? '30' : '3'}
 				stroke-linecap="round"
 				stroke-linejoin="round"
 				in:draw={{ duration: 1000, delay: index * 1000, easing: quintOut }}
