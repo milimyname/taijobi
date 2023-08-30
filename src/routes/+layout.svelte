ca388ac86a402c37d12ffab4862112c651b867e5
mili <kamil.maksudov2019@gmail.com>
2023-08-30 21:48:14 +0200

    feat: animated the feedbacks button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -49,6 +49,29 @@
 
 		$clickedFeedback = false;
 	}
+
+	let animationText = '';
+	let isReversing = false;
+
+	async function performAnimation() {
+		const texts = ['Feedback', 'Bug'];
+		const delay = 2000 / (texts[isReversing ? 0 : 1].length - 1);
+
+		for (let i = 0; i < texts[isReversing ? 0 : 1].length; i++) {
+			animationText = texts[isReversing ? 0 : 1].slice(0, i + 1);
+			await new Promise((resolve) => setTimeout(resolve, delay));
+		}
+
+		if (isReversing) {
+			isReversing = false;
+			performAnimation();
+		} else {
+			isReversing = true;
+			setTimeout(performAnimation, 2000);
+		}
+	}
+
+	performAnimation();
 </script>
 
 <!-- {#if audioUrl}
@@ -156,22 +179,9 @@
 
 <button
 	on:click={() => ($clickedFeedback = !$clickedFeedback)}
-	class="leave-feedback absolute left-[45%] top-14 z-[888] -translate-y-1/2 translate-x-1/2"
+	class="leave-feedback absolute left-[25%] top-10 z-[888] w-28 -translate-y-1/2 translate-x-1/2 rounded-full border px-4 py-2"
 >
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		fill="none"
-		viewBox="0 0 24 24"
-		stroke-width="1.5"
-		stroke="currentColor"
-		class="h-6 w-6"
-	>
-		<path
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
-		/>
-	</svg>
+	{animationText}
 </button>
 
 <slot />
