1c71dbbeb2fba258ffb178676b5da40bc9edf3ce
mili <kamil.maksudov2019@gmail.com>
2023-09-04 21:44:43 +0200

    bug: change username to other acc's username

--- src/routes/feedbacks/+page.server.ts
+++ src/routes/feedbacks/+page.server.ts
@@ -4,7 +4,7 @@ export const load = async ({ locals }) => {
 	const { id, username } = await locals.pb.authStore.model;
 
 	let feedbacks;
-	if (username === 'kj3')
+	if (username === 'users56993')
 		// Get all the flashcards
 		feedbacks = await locals.pb.collection('feedbacks').getFullList();
 	// Get all the flashcards
