1aa1ff863fda85fa109b87df0f5d9f1d8379a4e8
mili <kamil.maksudov2019@gmail.com>
2024-01-14 01:39:11 +0100

    fix: melt slider to shadcn slider

--- src/lib/components/ProgressSlider.svelte
+++ src/lib/components/ProgressSlider.svelte
@@ -12,10 +12,11 @@
 	import { clickOutside } from '$lib/utils/clickOutside';
 	import { spring } from 'svelte/motion';
 	import { getRandomNumber } from '$lib/utils/actions';
-	import { createSlider, melt } from '@melt-ui/svelte';
 	import { afterNavigate } from '$app/navigation';
-
+	import { Slider } from '$lib/components/ui/slider';
 	let progress = spring(1, { stiffness: 0.1, damping: 0.5 });
+	let max: number;
+	let value: number;
 
 	function getAlphabetLength() {
 		switch ($currentAlphabet) {
@@ -28,64 +29,35 @@
 		}
 	}
 
-	const {
-		elements: { root, range, thumb },
-		states: { value },
-		options: { max }
-	} = createSlider({
-		min: 1,
-		max: getAlphabetLength(),
-		step: 1,
-		onValueChange: (value) => {
-			$progress = value.curr[0];
-			$progressSlider = value.curr[0];
-
-			$searchKanji = '';
-			return value.next;
-		}
-	});
-
 	afterNavigate(() => {
 		$showProgressSlider = false;
 
 		$progress = getRandomNumber(1, getAlphabetLength());
 
-		$max = getAlphabetLength();
+		max = getAlphabetLength();
 
-		$progressSlider = $progress > $max ? getRandomNumber(1, getAlphabetLength()) : $progress;
+		$progressSlider = $progress > max ? getRandomNumber(1, getAlphabetLength()) : $progress;
 
 		$selectedKanjiGrade = '0';
 	});
 
-	$: $value[0] = $progressSlider;
+	$: value = $progressSlider;
 
-	$: $max = $kanjiLength;
+	$: max = $kanjiLength;
 </script>
 
 {#if $showProgressSlider}
-	<div
-		use:clickOutside={() => ($showProgressSlider = false)}
-		use:melt={$root}
-		class=" noselect relative z-40 mx-auto flex mt-auto mb-4 items-center rounded-full bg-black/40 w-full"
-	>
-		<div class=" block h-[52px] cursor-ew-resize sm:h-[56px]">
-			<div use:melt={$range} class="h-[52px] rounded-l-full bg-[#0A6EBD] shadow-xl sm:h-[56px]" />
-		</div>
-		<div
-			use:melt={$thumb()}
-			class="block h-[52px] w-2 rounded-full bg-white focus:ring-4 focus:ring-black/40 sm:h-[56px]"
+	<div use:clickOutside={() => ($showProgressSlider = false)} class="mt-auto mb-4">
+		<Slider
+			value={[value]}
+			onValueChange={(v) => {
+				$progress = v[0];
+				$progressSlider = v[0];
+
+				$searchKanji = '';
+			}}
+			{max}
+			step={1}
 		/>
 	</div>
 {/if}
-
-<style>
-	.noselect {
-		-webkit-touch-callout: none; /* iOS Safari */
-		-webkit-user-select: none; /* Safari */
-		-khtml-user-select: none; /* Konqueror HTML */
-		-moz-user-select: none; /* Old versions of Firefox */
-		-ms-user-select: none; /* Internet Explorer/Edge */
-		user-select: none; /* Non-prefixed version, currently
-                                  supported by Chrome, Edge, Opera and Firefox */
-	}
-</style>
---
+++ src/lib/components/ui/slider/slider.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Slider as SliderPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils';
+
+	type $$Props = SliderPrimitive.Props;
+
+	let className: $$Props['class'] = undefined;
+	export let value: $$Props['value'] = [0];
+	export { className as class };
+</script>
+
+<SliderPrimitive.Root
+	bind:value
+	class={cn('relative flex w-full touch-none select-none items-center', className)}
+	{...$$restProps}
+>
+	<span
+		class="relative h-14 touch-none select-none w-full grow overflow-hidden rounded-full bg-secondary"
+	>
+		<SliderPrimitive.Range class="absolute touch-none select-none h-full bg-primary" />
+	</span>
+	<SliderPrimitive.Thumb
+		class="block h-14 w-3 rounded-full border-2 touch-none select-none border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+	/>
+</SliderPrimitive.Root>
