4c38d21d852670aaae2be4367136c99f9e53f5eb
mili <kamil.maksudov2019@gmail.com>
2024-04-01 04:23:56 +0200

    feat: add dev pocketbase instance

--- src/lib/utils/pocketbase.ts
+++ src/lib/utils/pocketbase.ts
@@ -1,3 +1,6 @@
+import { dev } from '$app/environment';
 import PocketBase from 'pocketbase';
 
-export const pocketbase = new PocketBase(import.meta.env.VITE_POCKETBASE_URL);
+export const pocketbase = new PocketBase(
+	dev ? import.meta.env.VITE_DEV_POCKETBASE_URL : import.meta.env.VITE_POCKETBASE_URL
+);
